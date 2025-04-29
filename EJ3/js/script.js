0dc816c19f58cfc9db0afa30c8072f88a172d14e
Juan Fogel <juang.fogel@gmail.com>
2025-04-29 17:53:03 -0300

    ordene un poco

--- EJ3/js/script.js
+++
@@ -1,9 +0,0 @@
-function resaltarParrafos() {
-    const parrafos = document.querySelectorAll('.parrafo');
-    parrafos.forEach(p => p.classList.add('resaltado'));
-}
-
-function ocultarParrafos() {
-    const parrafos = document.querySelectorAll('.parrafo');
-    parrafos.forEach(p => p.classList.toggle('oculto'));
-}
