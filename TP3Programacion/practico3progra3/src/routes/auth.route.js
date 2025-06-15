a2c5c5acf7ee155ee623dd0ef5f9666715710cd9
unknown <estebanquiniones11@gmail.com>
2025-06-15 14:24:11 -0300

    modifico errores

--- TP3Programacion/practico3progra3/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, registro } = require('../controllers/auth.controller');
-const { validarAuth } = require('../validators/auth.validator');
-
-router.post('/login', validarAuth, login);
-router.post('/registro', validarAuth, registro);
-
-module.exports = router; 
